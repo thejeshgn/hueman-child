a736e0dd0207c898bf73b47c3e1bcdd94b1daa8e
Thejesh GN <i@thejeshgn.com>
2017-07-05 17:45:16 +0530

    Update header.php
    pointing rel me to /about page

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="profile" href="http://gmpg.org/xfn/11">
+  <link rel="me" href="https://thejeshgn.com/about">
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
   <?php wp_head(); ?>
